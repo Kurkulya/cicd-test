a0b161cefcfed644d2600a2b907337c94e720c75
Kurkulya <kulinich_v@hotmail.com>
2026-02-02 04:02:18 +0100

    ci: fix test issues

--- src/Counter.test.jsx
+++ src/Counter.test.jsx
@@ -1,13 +1,17 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import { Counter } from "./Counter";
 
+// eslint-disable-next-line no-undef
 test("renders initial value", () => {
   render(<Counter />);
+  // eslint-disable-next-line no-undef
   expect(screen.getByTestId("value")).toHaveTextContent("0");
 });
 
+// eslint-disable-next-line no-undef
 test("increments counter", () => {
   render(<Counter />);
   fireEvent.click(screen.getByText("+"));
+  // eslint-disable-next-line no-undef
   expect(screen.getByTestId("value")).toHaveTextContent("1");
 });
